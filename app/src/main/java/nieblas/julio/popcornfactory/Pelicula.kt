48f190c8a069034a106d3009308d82e1be65528d
JulioNieblas <julio.nieblas@potros.itson.edu.mx>
2021-03-06 00:42:02 -0700

    Commit para subir proyecto PopCornFactory

---
+++ app/src/main/java/nieblas/julio/popcornfactory/Pelicula.kt
@@ -0,0 +1,6 @@
+package nieblas.julio.popcornfactory
+
+data class Pelicula (var titulo: String,
+                     var image: Int,
+                     var header: Int,
+                     var sinopsis: String)
---
+++ app/src/main/java/nieblas/julio/popcornfactory/Welcome.kt
@@ -0,0 +1,11 @@
+package nieblas.julio.popcornfactory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Welcome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
